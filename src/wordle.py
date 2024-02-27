28b31c8dfebcfb316b69522e0ee55b99b1c6b1d9
vish.fish789 <vish.fish789@gmail.com>
2024-02-27 17:29:23 +0000

    did stuff

--- src/wordle.py
+++ src/wordle.py
@@ -1,4 +1,7 @@
 from enum import Enum
+import requests
+import random
+from collections import Counter
 
 class Matches(Enum):
     EXACT_MATCH = 'Exact Match'
@@ -42,20 +45,38 @@ def afterPartialMatches(target, guess, usedLetters, result, remainingLetters):
 def lettersLeft(allWordLetters, usedLetters):
     return [allWordLetters[i] for i in range(WORD_SIZE) if not usedLetters[i]]
 
+
 def tally(target, guess):
-    is5Letters(guess)
-    allWordLetters = list(target)
-    usedLetters = [False] * WORD_SIZE  
-    result = [Matches.NO_MATCH] * WORD_SIZE
+    is_5_letters(guess)
+    response = []
+    for i in range(WORD_SIZE):
+        response.append(tally_for_position(i, target, guess))
+    return response
+
+def tally_for_position(position, target, guess):
+    if target[position] == guess[position]:
+        return Matches.EXACT
     
-    exactMatches(target, guess, usedLetters, result)
-    remainingLetters = lettersLeft(allWordLetters, usedLetters)
-    partialMatches(target, guess, usedLetters, result, remainingLetters)
-    afterPartialMatches(target, guess, usedLetters, result, remainingLetters)
+    letter = guess[position]
+    positional_matches = count_positional_matches(target, guess, letter)
+    non_positional_occurrences_in_target = count_occurrences_until_position(WORD_SIZE - 1, target, letter) - positional_matches
+    occurrences_in_guess_until_position = count_occurrences_until_position(position, guess, letter)
+    
+    if non_positional_occurrences_in_target >= occurrences_in_guess_until_position:
+        return Matches.EXISTS
+    
+    return Matches.NO_MATCH
 
-    return result
+def count_positional_matches(target, guess, letter):
+    return sum(1 for t, g in zip(target, guess) if t == g and t == letter)
 
-#Feedback:  we can turn the above code into functional style. Please see the assign2reviews.txt for some suggestions.
+def count_occurrences_until_position(position, word, letter):
+    return Counter(word[:position + 1])[letter]
+
+def is_5_letters(guess):
+    if len(guess) != WORD_SIZE:
+        raise ValueError("Word must be 5 letters")
+
 
 def play(attempts, target, guess):
   tally_result = tally(target, guess)
@@ -83,3 +104,7 @@ def determine_game_status(attempts, tally_result):
     return GameStatus.WON
   
   return GameStatus.IN_PROGRESS
+
+def get_word(link):
+    words = requests.get(link)
+    return random.choice(words)
