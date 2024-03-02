485e2392c76b749521d986db6841fbb0489d86a5
vish.fish789@gmail.com <vish.fish789@gmail.com@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-02 05:25:29 +0000

    trying to fix the error

--- kodikanti_subramaniam/assign2/src/wordle.py
+++ kodikanti_subramaniam/assign2/src/wordle.py
@@ -76,9 +76,9 @@ def determine_message(attempts, tally_result):
   return ''
 
 def determine_game_status(attempts, tally_result):
-  if all(match == Matches.EXACT_MATCH for match in tally_result) and attempts <= 5:
+  if all(match == Matches.EXACT_MATCH for match in tally_result) and attempts < 5:
     return GameStatus.WON
-  elif attempts > 5:
+  elif attempts >= 5:
     return GameStatus.LOST
   else:
     return GameStatus.IN_PROGRESS
