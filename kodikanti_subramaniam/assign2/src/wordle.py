3b236c9bed627b844fe7b3f1f555517820a729c5
vish.fish789@gmail.com <vish.fish789@gmail.com@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-03 09:16:11 +0000

    added test

--- kodikanti_subramaniam/assign2/src/wordle.py
+++ kodikanti_subramaniam/assign2/src/wordle.py
@@ -56,7 +56,7 @@ def count_number_of_occurrences_until_position(position, word, letter):
 
 def play(attempts, target, guess):
   validate_trie(attempts)
-  
+  validate_spelling(target, guess)
   tally_result = tally(target, guess)
   
   message = determine_message(attempts, tally_result)
@@ -89,3 +89,7 @@ def validate_trie(attempts):
   
   if(attempts >= MAX_TRIES):
     raise Exception("Tries exceeded")
+  
+def validate_spelling(target, guess):
+  if(target != guess):
+    raise Exception("Wrong spelling")
