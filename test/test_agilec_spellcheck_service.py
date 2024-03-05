ad592a1fba316f6110f4912f09205b5d3f141a3d
svenkat <svenkat@cs.uh.edu>
2024-03-05 06:31:12 +0000

    secod test for get_response

--- test/test_agilec_spellcheck_service.py
+++ test/test_agilec_spellcheck_service.py
@@ -6,5 +6,8 @@ class AgilecSpellcheckServiceTests(unittest.TestCase):
     def test_get_response_returns_true_for_FAVOR(self):
         self.assertEqual(get_response("FAVOR"), "true")
 
+    def test_get_response_returns_false_for_FAVRR(self):
+        self.assertEqual(get_response("FAVRR"), "false")
+
 if __name__ == '__main__':
     unittest.main()
