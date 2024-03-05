0cfd5ca3c81523352adb3b633c6766ab809b017a
vish.fish789 <vish.fish789@gmail.com>
2024-03-05 07:00:44 +0000

    parse

--- test/test_agilec_spellcheck_service.py
+++ test/test_agilec_spellcheck_service.py
@@ -1,5 +1,5 @@
 import unittest
-from src.agilec_spellcheck_service import get_response
+from src.agilec_spellcheck_service import get_response, parse
 
 
 class AgilecSpellcheckServiceTests(unittest.TestCase):
@@ -9,5 +9,7 @@ class AgilecSpellcheckServiceTests(unittest.TestCase):
     def test_get_response_returns_false_for_FAVRR(self):
         self.assertEqual(get_response("FAVRR"), "false")
 
+    def test_parse_true_returns_True(self):
+        self.assertTrue(parse("true"))
 if __name__ == '__main__':
     unittest.main()
