313ffa9bf063f12f2d3f70a6a88d5ee51da18379
svenkat@cs.uh.edu <svenkat@cs.uh.edu@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-06 07:17:45 +0000

    assign2 synopsis: let's examine the test

--- kodikanti_subramaniam/assign2/src/agilec_spellcheck_service.py
+++ kodikanti_subramaniam/assign2/src/agilec_spellcheck_service.py
@@ -6,8 +6,8 @@ def get_response(word):
   return requests.get(f"{URL}{word}").text
 
 def parse(response):
-  if response not in ["true","false"]:
-    raise Exception("Invalid response")
+  if response not in ["true","false"]: #Feedback: space after comma, please
+    raise Exception("Invalid response") #Feedback: a blank line after this line, please
   return response == "true"
 
 def is_spelling_correct(word):
