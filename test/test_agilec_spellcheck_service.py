4bae5284ae550d36ae695d264d96d123d753e02b
svenkat <svenkat@cs.uh.edu>
2024-03-07 07:14:52 +0000

    assign2 synopsis: let's move forward to the next step

--- test/test_agilec_spellcheck_service.py
+++ test/test_agilec_spellcheck_service.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from src.agilec_spellcheck_service import get_response, parse, is_spelling_correct, get_a_random_word
+from src.agilec_spellcheck_service import get_response, parse, is_spelling_correct
 
 
 class AgilecSpellcheckServiceTests(unittest.TestCase):
@@ -35,35 +35,33 @@ class AgilecSpellcheckServiceTests(unittest.TestCase):
         mock_get_response.assert_called_once_with("FAVOR")
         mock_parse.assert_called_once_with('false')
 
-    #@patch('src.agilec_spellcheck_service.get_response', side_effect=Exception("Network Error"))
-    def test_is_spelling_correct_throws_network_error_if_getResponse_throws_that_exception(self):
-        def get_response_stub(word): #Feedback: please remove
-            raise Exception("Network Error")
+    @patch('src.agilec_spellcheck_service.get_response', side_effect=Exception("Network Error"))
+    def test_is_spelling_correct_throws_network_error_if_getResponse_throws_that_exception(self, mock_get_response):
+        self.assertRaisesRegex(Exception, "Network Error", is_spelling_correct, "FAVOR")
 
-        with patch("src.agilec_spellcheck_service.get_response", side_effect=get_response_stub):
-            self.assertRaisesRegex(Exception, "Network Error", is_spelling_correct, "FAVOR")
-
+#Feedback: please remove feedback notes and commented out stuff
         #Feedback: Good work on this test. We can make this simpler.
         #@patch('src.agilec_spellcheck_service.get_response', side_effect=Exception("Network Error"))
         #then within the test we can call self.assertRaisesRegex...
-        
-    @patch("src.agilec_spellcheck_service.requests.get")
-    def test_getResponse_returns_list_of_words(self, mock_get):
-        mock_get.return_value.text = "FAVOR"
-        words = get_response("http://agilec.cs.uh.edu/spellcheck")
-        self.assertTrue("FAVOR" in words)
 
-    
-    def test_getARandomWord_given_list_of_words(self):
-        word_list = ["FAVOR", "SKILL", "APPLE"]
-        random_word = get_a_random_word(word_list)
-        self.assertIn(random_word, word_list)
-
-    def test_getARandomWord_returns_two_different_words(self):
-        word_list = ["FAVOR", "SKILL", "APPLE"]
-        random_word1 = get_a_random_word(word_list)
-        random_word2 = get_a_random_word(word_list)
-        self.assertNotEqual(random_word1, random_word2)
+#Feedback:  please remove the following
+#    @patch("src.agilec_spellcheck_service.requests.get")
+#    def test_getResponse_returns_list_of_words(self, mock_get):
+#        mock_get.return_value.text = "FAVOR"
+#        words = get_response("http://agilec.cs.uh.edu/spellcheck")
+#        self.assertTrue("FAVOR" in words)
+#
+#    
+#    def test_getARandomWord_given_list_of_words(self):
+#        word_list = ["FAVOR", "SKILL", "APPLE"]
+#        random_word = get_a_random_word(word_list)
+#        self.assertIn(random_word, word_list)
+#
+#    def test_getARandomWord_returns_two_different_words(self):
+#        word_list = ["FAVOR", "SKILL", "APPLE"]
+#        random_word1 = get_a_random_word(word_list)
+#        random_word2 = get_a_random_word(word_list)
+#        self.assertNotEqual(random_word1, random_word2)
     
         
 if __name__ == '__main__':
