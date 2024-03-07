5be2ba4946657f35aa695b4c4eaeb355221420ce
svenkat@cs.uh.edu <svenkat@cs.uh.edu@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-07 07:14:52 +0000

    assign2 synopsis: let's move forward to the next step

--- kodikanti_subramaniam/assign2/src/agilec_spellcheck_service.py
+++ kodikanti_subramaniam/assign2/src/agilec_spellcheck_service.py
@@ -15,5 +15,6 @@ def parse(response):
 def is_spelling_correct(word):
   return parse(get_response(word))
 
-def get_a_random_word(word_list):
-  return random.choice(word_list)
+#Feedback: please remove this from here. Single Responsibilty Princple. This funtion below is not related to anything above. It should be in its own file
+#def get_a_random_word(word_list):
+#  return random.choice(word_list)
