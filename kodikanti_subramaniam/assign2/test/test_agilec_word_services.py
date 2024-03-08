04899571688ca33c64ed264d7b85ebf3ba8baf92
vish.fish789@gmail.com <vish.fish789@gmail.com@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-08 23:33:40 +0000

    did it work

---
+++ kodikanti_subramaniam/assign2/test/test_agilec_word_services.py
@@ -0,0 +1,10 @@
+import unittest
+import unittest
+import requests
+from unittest.mock import patch
+from src.agilec_word_services import get_response
+
+class AgilecWordServicesTests(unittest.TestCase):
+    def test_get_response_returns_list_of_words(self):
+        self.assertEqual(['FAVOR', 'SMART', 'GUIDE', 'TESTS', "GRADE", "BRAIN", "SPAIN", "SPINE", "GRAIN", "BOARD"], get_response)
+        
