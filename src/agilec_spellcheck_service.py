5fef41e15e9a2e1e5aaf123fc80f825310b1780d
vish.fish789 <vish.fish789@gmail.com>
2024-03-08 02:13:57 +0000

    not done

--- src/agilec_spellcheck_service.py
+++ src/agilec_spellcheck_service.py
@@ -1,5 +1,4 @@
 import requests
-import random
 
 def get_response(word):
   URL = "http://agilec.cs.uh.edu/spellcheck?check="
@@ -14,7 +13,3 @@ def parse(response):
 
 def is_spelling_correct(word):
   return parse(get_response(word))
-
-#Feedback: please remove this from here. Single Responsibilty Princple. This funtion below is not related to anything above. It should be in its own file
-#def get_a_random_word(word_list):
-#  return random.choice(word_list)
