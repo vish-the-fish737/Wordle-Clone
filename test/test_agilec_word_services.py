cc0cd7d3609364e12d9b3b7c90426b2a13d9ecc5
vish.fish789 <vish.fish789@gmail.com>
2024-03-08 23:58:38 +0000

    ,

--- test/test_agilec_word_services.py
+++ test/test_agilec_word_services.py
@@ -5,5 +5,5 @@ from src.agilec_word_services import get_response
 
 class AgilecWordServicesTests(unittest.TestCase):
     def test_get_response_returns_list_of_words(self):
-        self.assertEqual(['FAVOR', 'SMART', 'GUIDE', 'TESTS', "GRADE", "BRAIN", "SPAIN", "SPINE", "GRAIN", "BOARD"], get_response)
+        self.assertEqual(['FAVOR', 'SMART', 'GUIDE', 'TESTS', "GRADE", "BRAIN", "SPAIN", "SPINE", "GRAIN", "BOARD", ''], get_response())
         
