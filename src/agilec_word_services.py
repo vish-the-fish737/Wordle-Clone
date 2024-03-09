c0b65dd3eca74f14284618c355d0eeddb93129a1
vish.fish789 <vish.fish789@gmail.com>
2024-03-09 00:45:26 +0000

    removed

--- src/agilec_word_services.py
+++ src/agilec_word_services.py
@@ -1,10 +1,7 @@
 import requests
 
 def get_response():
-  URL = "http://agilec.cs.uh.edu/words" #Feedback: a blank line after this line, please
-  words = requests.get(URL)
-  wordlist = words.text.split("\n")
+  URL = "http://agilec.cs.uh.edu/words"
   
-  return wordlist
-  #Feedback: return requests.get(URL).text.split("\n")
+  return requests.get(URL).text.split("\n")
   
