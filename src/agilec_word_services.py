fa98c70f06f4e4c6cce65e99cdc9e0be4ab7efd8
vish.fish789 <vish.fish789@gmail.com>
2024-03-09 01:16:34 +0000

    now i can start the gui

--- src/agilec_word_services.py
+++ src/agilec_word_services.py
@@ -1,7 +1,10 @@
 import requests
+import random
 
 def get_response():
   URL = "http://agilec.cs.uh.edu/words"
   
   return requests.get(URL).text.split("\n")
   
+def get_a_random_word(word_list, seed=None):
+    return random.choice(word_list)
--- test/test_agilec_word_services.py
+++ test/test_agilec_word_services.py
@@ -1,9 +1,23 @@
 import unittest
 import requests
 from unittest.mock import patch
-from src.agilec_word_services import get_response
+from src.agilec_word_services import get_response, get_a_random_word
 
 class AgilecWordServicesTests(unittest.TestCase):
     def test_get_response_returns_list_of_words(self):
         self.assertTrue(len(get_response()) > 0)
+        
+    def test_get_a_random_word_given_list_of_words(self):
+        word_list = get_response()
+        random_word = get_a_random_word(word_list)
+        
+        self.assertIn(random_word, word_list)
+
+    def test_getARandomWord_returns_two_different_words(self):
+        word_list = get_response()
+        
+        random_word1 = get_a_random_word(word_list, seed=1001)
+        random_word2 = get_a_random_word(word_list, seed=1002)
+        
+        self.assertNotEqual(random_word1, random_word2)
         
