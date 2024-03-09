7b0b7108dea03b3abe5dc397ce0d4ec3a429e38d
svenkat <svenkat@cs.uh.edu>
2024-03-09 00:25:05 +0000

    assign2 synopsis: let's move forward

--- src/agilec_word_services.py
+++ src/agilec_word_services.py
@@ -1,8 +1,10 @@
 import requests
 
 def get_response():
-  URL = "http://agilec.cs.uh.edu/words"
+  URL = "http://agilec.cs.uh.edu/words" #Feedback: a blank line after this line, please
   words = requests.get(URL)
   wordlist = words.text.split("\n")
   
   return wordlist
+  #Feedback: return requests.get(URL).text.split("\n")
+  
--- test/test_agilec_word_services.py
+++ test/test_agilec_word_services.py
@@ -6,4 +6,6 @@ from src.agilec_word_services import get_response
 class AgilecWordServicesTests(unittest.TestCase):
     def test_get_response_returns_list_of_words(self):
         self.assertEqual(['FAVOR', 'SMART', 'GUIDE', 'TESTS', "GRADE", "BRAIN", "SPAIN", "SPINE", "GRAIN", "BOARD", ''], get_response())
+        #Feedback: the words on the service may change and this test will break.
+        #instead assertTrue(len(get_response()) > 0)
         
