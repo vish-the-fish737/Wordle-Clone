378e10494a55fc52e815a66f9d82546e6f61cd19
vish.fish789@gmail.com <vish.fish789@gmail.com@d2b48808-ef0e-411a-a9e2-c64d022a86f0>
2024-03-10 07:54:58 +0000

    just need spellcheck

--- kodikanti_subramaniam/assign2/src/agilec_word_services.py
+++ kodikanti_subramaniam/assign2/src/agilec_word_services.py
@@ -7,7 +7,8 @@ def get_response():
   
   return requests.get(URL).text.split("\n")
   
-def get_a_random_word(word_list, seed=None): #Feedback: words instead of word_list. time.....ns instead of None
-    random.seed(seed) #Feedback: a blank line after this line, please
-    return random.choice(word_list)
+def get_a_random_word(words, seed=time.time_ns):
+    random.seed(seed)
+    
+    return random.choice(words)
   
